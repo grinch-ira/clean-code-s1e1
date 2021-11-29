ad4c3b6c08bbb33633c23c9796d791f7921bf2a5
Hryniuk Iryna <grinch.ira@gmail.com>
2021-11-29 15:16:32 +0300

    refactor: using tag selectors, padding in media blocks and separating selectors and properties 3.4 3.5 3.8

--- app.js
+++ app.js
@@ -33,18 +33,21 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task label-task';
+    listItem.className = "todo-item";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className = "input-checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="btn edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = "img";
     deleteButton.appendChild(deleteButtonImg);
 
 
